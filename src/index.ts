e90789fdac0823548aa27228ac5733a5ed6a1635
Bernardo J. C. Nunes <nunes.bjc@gmail.com>
2024-05-09 22:17:17 +0200

    chore: renamed Error type E from previous 'T', just for consistency

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ export function Ok<T>(value: T): _Ok<T> {
   return new _Ok(value);
 }
 
-export function Err<T>(value: T): _Err<T> {
-  return new _Err(value);
+export function Err<E>(error: E): _Err<E> {
+  return new _Err(error);
 }
