854ff3e4bf46a2da0ec2cc9ee5eeda51bd58a00b
Bernardo J. C. Nunes <nunes.bjc@gmail.com>
2024-05-19 21:50:10 +0200

    fix: equality spec

--- tests/equality.spec.ts
+++ tests/equality.spec.ts
@@ -4,7 +4,7 @@ describe('equality', () => {
   it('should not equal similar results', () => {
     const r1 = Ok('some value');
     const r2 = Ok('some value');
-    expect(r1).not.toBe(r2);
-    expect(r1).not.toEqual(r2);
+    expect(r1).toEqual(r2); // similar...
+    expect(r1).not.toBe(r2); // but not the same...
   });
 });
