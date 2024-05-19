c547e7962e385fa41ab82643d6b8fc8c4f6b08e6
Bernardo J. C. Nunes <nunes.bjc@gmail.com>
2024-05-19 21:04:34 +0200

    chore: simplified exported API

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,14 @@ import * as Result from './result';
 
 export * from './error/UnwrapError';
 
-export function Ok(): Result.Ok<void>;
-export function Ok(value: undefined): Result.Ok<void>;
 export function Ok<T>(value: T): Result.Ok<T>;
+export function Ok(): Result.Ok<void>;
 export function Ok<T>(value?: T): Result.Ok<T | void> {
   return new Result.Ok(value);
 }
 
-export function Err(): Result.Err<void>;
-export function Err(error: undefined): Result.Err<void>;
 export function Err<E>(error: E): Result.Err<E>;
+export function Err(): Result.Err<void>;
 export function Err<E>(error?: E): Result.Err<E | void> {
   return new Result.Err(error);
 }
